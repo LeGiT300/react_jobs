e112da353b7ba02cd500539dd176f8388e1af364
Leku Genuine Tita <titaleku003@gmail.com>
2025-03-11 14:01:39 +0100

    Add initial app layout and home screen components with dependencies

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Hello Guys... welcome to my YouTube channel</Text>
+        </View>
+    )
+}
+
+export default Home;
